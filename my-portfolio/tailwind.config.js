7bc20e941d1292d5c9ee6c1a7245cdf8459915f6
razvan-andrei-zamfir <razvan.zamfir.986@gmail.com>
2024-10-23 15:11:30 +0300

    first commit

---
+++ my-portfolio/tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,jsx}",
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter var", ...defaultTheme.fontFamily.sans],
+      },
+    },
+  },
+  plugins: [],
+}
